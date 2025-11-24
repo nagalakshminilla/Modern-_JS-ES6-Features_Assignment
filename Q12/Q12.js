a130f8a83fd0a7b1b546e36bcae637b8886cfe51
nagalakshminilla <nillanaga1806@gmail.com>
2025-11-24 18:08:05 +0530

    Update Q12.js

--- Q12/Q12.js
+++ Q12/Q12.js
@@ -1,5 +1,5 @@
 const calculatePowers = (num) => ({ square: num * num, cube: num * num * num });
-// Test cases
+
 console.log(calculatePowers(5))
 console.log(calculatePowers(2))
 console.log(calculatePowers(3))
