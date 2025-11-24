aa6a7a07dab0f77fc3f282a54d21b8238ade3785
nagalakshminilla <nillanaga1806@gmail.com>
2025-11-24 07:37:37 -0500

    Add files via upload

---
+++ Q12/Q12.js
@@ -0,0 +1,6 @@
+const calculatePowers = (num) => ({ square: num * num, cube: num * num * num });
+// Test cases
+console.log(calculatePowers(5))
+console.log(calculatePowers(2))
+console.log(calculatePowers(3))
+console.log(calculatePowers(10))
